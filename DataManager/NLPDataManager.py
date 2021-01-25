7aedb7c035dd51e5880a0e21f025bf38d6a26dde
YangHan-Morningstar <imuyanghan@gmail.com>
2021-01-25 20:03:51 +0800

    添加模型测试时数据处理函数以及修正词典计算方法

--- DataManager/NLPDataManager.py
+++ DataManager/NLPDataManager.py
@@ -1,6 +1,9 @@
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
 from DataManager.ToolKit import ToolKit
 import os
 import jieba
+import numpy as np
 
 
 class NLPDataManager(ToolKit):
@@ -28,12 +31,17 @@ class NLPDataManager(ToolKit):
                 else:
                     self.word_frequency[word] += 1
 
-    def cal_word_dict(self, top_k=5):
+    def cal_word_dict(self, top_k=20000):
         print("Calculating word dict...")
 
-        for word, fre in self.word_frequency.items():
-            if fre > top_k:
-                if word not in self.word_dict:
+        word_frequency_sorted = sorted(self.word_frequency, key=lambda x: x[1], reverse=True)
+
+        if len(word_frequency_sorted) <= top_k:
+            for word, fre in word_frequency_sorted:
+                    self.word_dict[word] = len(self.word_dict)
+        else:
+            word_frequency_sorted_maxlen_is_top_k = word_frequency_sorted[0: top_k]
+            for word, fre in word_frequency_sorted_maxlen_is_top_k:
                     self.word_dict[word] = len(self.word_dict)
 
     def get_word_dict(self):
@@ -50,16 +58,11 @@ class NLPDataManager(ToolKit):
 
         counter = 0
         for filepath, label in self.filepath_label_dict.items():
-            txt_content_num = []
             features_label_dict = {}
             txt_content = self.get_txt_content_from_text_file(filepath)
             word_list = self.cut_sentence_to_word_and_cal_maxlen(txt_content)
 
-            for word in word_list:
-                if word in self.word_dict:
-                    txt_content_num.append(self.word_dict[word])
-                else:
-                    txt_content_num.append(self.word_dict["NaN"])
+            txt_content_num = self.transform_word_to_num(word_list)
 
             features_label_dict["data_features"] = txt_content_num
             features_label_dict["label"] = label
@@ -67,6 +70,22 @@ class NLPDataManager(ToolKit):
             self.write_to_json(self.target_path + "/" + str(counter) + ".json", features_label_dict)
 
             counter += 1
+
+    def get_model_input_data_on_single_sentence(self, sentence, maxlen=None):
+        word_list = self.cut_sentence_to_word_and_cal_maxlen(sentence)
+        txt_content_num = self.transform_word_to_num(word_list)
+        data = np.array(pad_sequences(txt_content_num, maxlen=maxlen, padding="post")).tolist()
+        return data
+
+    def transform_word_to_num(self, word_list):
+        temp_num_list = []
+        for word in word_list:
+            if word in self.word_dict:
+                temp_num_list.append(self.word_dict[word])
+            else:
+                temp_num_list.append(self.word_dict["NaN"])
+
+        return temp_num_list
 
     def get_txt_content_from_text_file(self, txt_filepath):
         f = open(txt_filepath, 'r')
