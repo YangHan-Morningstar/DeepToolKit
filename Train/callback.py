b14b2449ad628922ca9bb12b6fbbe2c1f07023d4
YangHan-Morningstar <imuyanghan@gmail.com>
2021-01-27 15:56:04 +0800

    添加tensorboard_callback

--- Train/callback.py
+++ Train/callback.py
@@ -1,5 +1,8 @@
 from tensorflow.keras.callbacks import ModelCheckpoint
+import tensorflow as tf
 import os
+import datetime
+import shutil
 
 
 class CallBack(object):
@@ -18,3 +21,10 @@ class CallBack(object):
                                                       save_weights_only=True)
 
         return ck_callback
+
+    def get_tensorboard_callback(self):
+        shutil.rmtree("./logs")
+        log_dir = "logs/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+        tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+
+        return tensorboard_callback
